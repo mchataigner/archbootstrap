917387115b515704f5562ca8f32a4374b874fcee
Mathieu Chataigner <m.chataigner@criteo.com>
2016-09-14 19:52:07 +0200

    Add grub support

--- bootstrap.py
+++ bootstrap.py
@@ -5,6 +5,8 @@ import os.path
 import http.client
 import sys
 import getopt
+from collections import defaultdict
+import subprocess
 
 RE_INSTALL = False
 
@@ -50,6 +52,7 @@ def install_required_packages():
     os.system("pacman -S --noconfirm --needed base-devel")
     os.system("pacman -S --noconfirm --needed linux-tools")
     os.system("pacman -S --noconfirm --needed git python curl")
+    os.system("pacman -S --noconfirm --needed grub")
 
 def setup_sudoers():
     if os.path.isfile("/etc/sudoers.d/admin") and not RE_INSTALL:
@@ -133,8 +136,88 @@ def build(flavors = list()):
         os.system("yaourt -Pi --noconfirm moot-prog")
     os.chdir("..")
 
+def setup_mkinitcpio():
+    os.system("sed -i 's/^MODULES=.*/MODULES=\"ext4\"/g' /etc/mkinitcpio.conf")
+    os.system("sed -i 's/^HOOKS=.*/HOOKS=\"base udev autodetect modconf block keyboard keymap encrypt filesystems fsck\"/g' /etc/mkinitcpio.conf")
+    os.system("sed -i 's/^#COMPRESSION=\"lz4\"/COMPRESSION=\"lz4\"/g' /etc/mkinitcpio.conf")
+    os.system("mkinitcpio -p linux")
+
+def setup_grub():
+    def abs_p(p):
+        while os.path.islink(p):
+            p=os.path.abspath(os.path.dirname(p) + "/" + os.readlink(p))
+        return p
+    def rename(p):
+        return p.replace("../../", "/dev/")
+    def clean_uuid(uuid):
+        return uuid.replace("-","")
+    disks_by_uuid = defaultdict(list)
+    disks_by_device = dict()
+    uuid="/dev/disk/by-uuid/"
+    for p in os.listdir(uuid):
+        if os.path.islink(uuid + p):
+            disks_by_device[abs_p(uuid + p)] = (clean_uuid(p))
+            disks_by_uuid[clean_uuid(p)].append(p)
+            disks_by_uuid[clean_uuid(p)].append(abs_p(uuid + p))
+    label = "/dev/disk/by-label/"
+    for p in os.listdir(label):
+        if os.path.islink(label + p):
+            disks_by_uuid[disks_by_device[abs_p(label + p)]].append(p)
+    dms = dict()
+    for l in subprocess.check_output(["dmsetup", "ls"]).decode().strip().split("\n"):
+        dm = l.split("\t")[0]
+        for ll in subprocess.check_output(["dmsetup", "info", dm]).decode().strip().split("\n"):
+            if ll.startswith("UUID:") and "CRYPT-LUKS" in ll:
+                crypted_uuid = ll.split("-")[2]
+                info = list(disks_by_uuid[crypted_uuid])
+                info.append(dm)
+                dms[abs_p("/dev/mapper/" + dm)] = info
+                break
+    dm_list = list(dms.items())
+    if len(dm_list) > 1:
+        index = int(input('which one is root?\n' + '\n'.join(map(lambda x: str(x[0]+1)+" : "+str(x[1][0]) + " - " + str(x[1][1][0]) + " - " + str(x[1][1][1]), enumerate(dm_list)))+'\n'))
+        dm = dm_list[index - 1]
+    elif len(dm_list) == 1:
+        dm = dm_list[0]
+    else:
+        dm = None
+    if dm:
+        crypt_uuid = dm[1][0]
+        crypt_name = dm[1][-1]
+        os.system("sed -i 's#GRUB_CMDLINE_LINUX=.*#GRUB_CMDLINE_LINUX=\"cryptdevice=/dev/disk/by-uuid/"+crypt_uuid+":"+crypt_name+":allow-discards cryptkey=/dev/disk/by-uuid/648d19c6-3839-42d9-be47-00cfb73b62cc:ext4:/japan.jpg LOGLEVEL=5 libata.force=noncq\"#g' /etc/default/grub")
+    efi = False
+    with open("/proc/mounts", 'r') as f:
+        content = f.read()
+        for l in content.strip().split("\n"):
+            if l.startswith("efivarfs"):
+                efi = True
+            if " / " in l:
+                root_dev = abs_p(l.split(" ")[0])
+                while root_dev in dms:
+                    root_dev = dms[root_dev][1]
+    base_name = os.path.basename(root_dev)
+    matched_dev = list()
+    for i in os.listdir("/dev"):
+        if base_name.startswith(i):
+            matched_dev.append(i)
+    if len(matched_dev) > 1:
+        for i in matched_dev:
+            if i != base_name:
+                install_disk = "/dev/" + i
+                break
+    else:
+        install_disk = "/dev/" + matched_dev[0]
+    print(install_disk)
+    print(efi)
+
+    subprocess.check_output(["grub-mkconfig", "-o", "/boot/grub/grub.cfg"])
+    if efi:
+        subprocess.check_output(["grub-install", "--efi-directory=/boot/EFI", "--target=x86_64-efi", install_disk])
+    else:
+        subprocess.check_output(["grub-install", "--target=x86_64", install_disk])
+
 if __name__ == "__main__":
-    optlist, flavors = getopt.getopt(sys.argv[1:], "i")
+    optlist, flavors = getopt.getopt(sys.argv[1:], "ig")
     opts = dict(optlist)
     os.environ["TMPDIR"] = "/tmp"
     if os.path.isdir("/home/admin/archbootstrap"):
@@ -145,6 +228,9 @@ if __name__ == "__main__":
     if os.getuid() != 0:
         raise PermissionError("must run as root")
     install_required_packages()
+    if "-g" in opts:
+        setup_mkinitcpio()
+        setup_grub()
     create_base_user("m.chataigner", sgroups = ["sudo", "wheel"], passwd = True)
     create_base_user("admin")
     setup_sudoers()
