f340f5bb6d21e4b00a6ec4dcbdab3655f34fa565
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2019-08-11 14:51:23 +0200

    merge setup into one phase

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 install_required_packages() {
-	pacman -Syu --noconfirm
-	pacman -S --noconfirm --needed base
-	pacman -S --noconfirm --needed base-devel
-	pacman -S --noconfirm --needed linux-tools
-	pacman -S --noconfirm --needed git python curl ca-certificates-utils zsh
+	pacman -Syu --noconfirm --needed base base-devel linux-tools git python curl ca-certificates-utils zsh
 	update-ca-trust
 }
 
 install_required_packages
 curl -o bootstrap.py https://raw.githubusercontent.com/mchataigner/archbootstrap/master/bootstrap.py
-python bootstrap.py $@
+python bootstrap.py "$@"
